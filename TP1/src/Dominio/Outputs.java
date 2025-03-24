38691116e74cf82a19b3c6ca9e406ad5c6c59fcb
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-03-24 20:45:03 -0300

    first commit

---
+++ TP1/src/Dominio/Outputs.java
@@ -0,0 +1,10 @@
+package Dominio;
+
+public class Outputs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TP1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EmpleadosLay {
+}
