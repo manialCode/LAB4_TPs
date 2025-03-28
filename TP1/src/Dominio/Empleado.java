7d6626b46ca026c60adcdb71fbd7072d6e6568c8
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-03-28 11:05:18 -0300

    feat(class):Agregar constructores, getters y setters a la clase Empleado

--- TP1/src/Dominio/Empleado.java
+++ TP1/src/Dominio/Empleado.java
@@ -1,9 +1,60 @@
 package Dominio;
 
 public class Empleado {
-    private int id;
-    private String nombre;
-    private int edad;
 
-    static int uid = 1000;
+	private int id;
+	private String nombre;
+	private int edad;
+
+	static int cont = 1000;
+
+	// CONSTRUCTORES
+
+	public Empleado() {
+		cont++;
+		id = cont;
+		nombre = "Sin Nombre";
+		edad = 99;
+	}
+
+	public Empleado(String nombre, int edad) {
+		cont++;
+		id = cont;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	// GETTERS Y SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public static int devuelveProximoID() {
+		return cont + 1;
+	}
+
+	public String toString() {
+		return "ID del Empleado=" + id + ", Nombre=" + nombre + ", Edad=" + edad;
+	}
+
 }
